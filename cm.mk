c19f79b852146bab1187bc4e946ca07543b61999
LinhPhi <linhphi9x94@gmail.com>
2015-09-16 01:12:04 +0700

    Convert GreenOS to build CM

--- cm.mk
+++ cm.mk
@@ -1,22 +1,22 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/greenos/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/greenos/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/greenos/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/pantech/ef46l/ef46l.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ef46l
-PRODUCT_NAME := greenos_ef46l
+PRODUCT_NAME := cm_ef46l
 PRODUCT_BRAND := pantech
 PRODUCT_MODEL := Vega Racer 2
 PRODUCT_MANUFACTURER := PANTECH
